1aad17e4ef3f08e2732aee03f8d16c8497f449c3
Qi Wang <qi@broadinstitute.org>
2019-05-05 15:55:54 -0400

    attempt to add automation as submodule
    temp

--- automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookPyKernelSpec.scala
+++ automation/src/test/scala/org/broadinstitute/dsde/workbench/leonardo/notebooks/NotebookPyKernelSpec.scala
@@ -6,7 +6,6 @@ import org.broadinstitute.dsde.workbench.service.util.Tags
 import org.scalatest.DoNotDiscover
 
 import scala.concurrent.duration.DurationLong
-import scala.language.postfixOps
 
 /**
   * This spec verifies notebook functionality specifically around the Python 2 and 3 kernels.
